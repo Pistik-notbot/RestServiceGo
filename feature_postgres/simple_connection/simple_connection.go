e90074c5d6d12791c5c504fc53a148ef748a4300
Pistik-notbot <ykovlev105@gmail.com>
2026-04-17 21:06:48 +0500

    make table succesful

--- feature_postgres/simple_connection/simple_connection.go
+++ feature_postgres/simple_connection/simple_connection.go
@@ -8,15 +8,9 @@ import (
 
 func CheckConnection(ctx context.Context) (*pgx.Conn, error) {
 
-	conn, err := pgx.Connect(ctx, "postgres://postgres:123@localhost:5432/postgres")
+	conn, err := pgx.Connect(ctx, "postgres://postgres:1234@localhost:5432/postgres")
 	if err != nil {
 		return nil, err
 	}
-	defer conn.Close(ctx)
-
-	if err := conn.Ping(ctx); err != nil {
-		panic(err)
-	}
-
 	return conn, nil
 }
