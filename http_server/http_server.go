8dcd7df5d1bcdaf610903f30f1db00bfbfb097b4
Pistik-notbot <ykovlev105@gmail.com>
2026-05-18 21:33:12 +0500

    Start docker

---
+++ http_server/http_server.go
@@ -0,0 +1,20 @@
+package http_server
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func StartHTTPServer() error {
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Обработка запроса /ping")
+		w.Write([]byte("Привет docker\n"))
+	})
+
+	err := http.ListenAndServe(":5050", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		return nil
+	}
+	return err
+}
