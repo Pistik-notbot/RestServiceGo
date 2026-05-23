82679d825c5bb1029cc63f127342b8041771303f
Pistik-notbot <ykovlev105@gmail.com>
2026-05-23 23:39:53 +0500

    update dockerfile and add docker compose

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
-
-	"go.mod/http_server"
+	"os"
 )
 
 func main() {
-	if err := http_server.StartHTTPServer(); err != nil {
-		fmt.Printf("Ошибка при запуске HTTP сервера: %v\n", err)
+	_, err := os.Create("out/newfile.txt")
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println("HTTP сервер успешно запущен на порту 5050")
 
 }
